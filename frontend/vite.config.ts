409f4895ea4e3a266085dea978dcc93c1d59c4e9
mitsimi <noah.fuereder@gmail.com>
2025-05-14 15:30:36 +0200

    tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],server: {
+  plugins: [vue(), tailwindcss()],
+  server: {
     proxy: {
       "/video": {
         target: process.env.VITE_BACKEND_URL || "http://localhost:5000",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+    },
+  },
+});
